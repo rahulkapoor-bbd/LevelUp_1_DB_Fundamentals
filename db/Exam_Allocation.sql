cdb81eb0218c2170c7368e5c2a6a23917d2a86bb
Jesse van der Merwe <jesse@bbd.co.za>
2023-02-20 18:57:05 +0200

    Added my tables and constraints
    - DegreeCourse
    - Module_Degree_Bridge
    - Module

--- db/Exam_Allocation.sql
+++ db/Exam_Allocation.sql
@@ -1,4 +1,38 @@
-use Master;
+use ExamAllocation;
 go
 create database ExamAllocation;
 go
+
+CREATE TABLE DegreeCourse(
+	degree_code varchar(10) NOT NULL PRIMARY KEY,
+	degree_name varchar(200) NOT NULL,
+	duration smalldatetime NOT NULL,
+);
+GO 
+
+CREATE TABLE Module_Degree_Bridge(
+	module_degree_bridgeID int identity(1,1) NOT NULL PRIMARY KEY,
+	degree_code varchar(10) NOT NULL,
+	module_code varchar(10) NOT NULL,
+);
+GO
+
+CREATE TABLE Module(
+	module_code varchar(10) NOT NULL PRIMARY KEY,
+	module_name varchar(50) NOT NULL, 
+	semester_type char(1) NOT NULL,
+);
+GO
+
+ALTER TABLE Module_Degree_Bridge
+ADD FOREIGN KEY(module_code) REFERENCES Module(module_code)
+GO 
+
+ALTER TABLE Module_Degree_Bridge
+ADD FOREIGN KEY(degree_code) REFERENCES DegreeCourse(degree_code)
+GO 
+
+ALTER TABLE Module
+ADD CONSTRAINT check_semester_type
+	CHECK (semester_type = 'f' OR semester_type = 's' OR semester_type = 'y')
+GO
